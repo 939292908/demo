9b970fc20a031227be66ffab4d977d2d27b317c7
HT <30796475+github-HT@users.noreply.github.com>
2020-04-18 16:02:54 +0800

    新增历史成交

--- src/views/components/orderList/historyTrade.js
+++ src/views/components/orderList/historyTrade.js
@@ -1,15 +1,180 @@
 var m = require("mithril")
+let obj = {
+    list: [],
+    theadList: [
+        {
+            title: '合约',
+            class: ""
+        },{
+            title: '交易类型',
+            class: ""
+        }, {
+            title: '成交均价',
+            class: ""
+        }, {
+            title: '成交数量',
+            class: ""
+        }, {
+            title: '平仓盈亏',
+            class: ""
+        }, {
+            title: '手续费',
+            class: ""
+        }, {
+            title: '成交时间',
+            class: ""
+        },{
+            title: '仓位ID',
+            class: ""
+        }
+    ],
+    //初始化全局广播
+    initEVBUS: function () {
+        let that = this
+
+        // if (this.EV_WLT_POS_ORDER_UPD_unbinder) {
+        //     this.EV_WLT_POS_ORDER_UPD_unbinder()
+        // }
+        // this.EV_WLT_POS_ORDER_UPD_unbinder = window.gEVBUS.on(gTrd.EV_WLT_POS_ORDER_UPD, arg => {
+        //     that.initObj()
+        //     that.subPosNeedSymTick()
+        // })
+
+
+    },
+    //删除全局广播
+    rmEVBUS: function () {
+        if (this.EV_WLT_POS_ORDER_UPD_unbinder) {
+            this.EV_WLT_POS_ORDER_UPD_unbinder()
+        }
+    },
+    getHistoryList: function(){
+        let that = this
+        let s = window.gTrd
+        let aType = '01'
+        let Sym = window.gMkt.CtxPlaying.Sym
+        let AssetD = window.gMkt.AssetD[Sym] || {}
+        if(AssetD.TrdCls == 1){
+            aType = '02'
+        }else{
+            aType = '01'
+        }
+        let uid = s.RT["UserId"]
+        let isReq = s.trdInfoStatus.trade[aType]
+        if(!uid || !s || isReq) return
+        s.ReqTrdGetTrades({
+            AId: uid+aType,
+        }, function(aTrd, aArg){
+            if(aArg.code == 0){
+                s.trdInfoStatus.trade[aType] = 1
+                s.MyTrades[aType] = aArg.data
+                that.initObj()
+            }
+        })
+    },
+    initObj: function(){
+        let trade = window.gTrd.MyTrades['01']
+        console.log(trade)
+        let list = []
+        for(let k of trade){
+            let item = {}
+            let Sym = k.Sym
+            let ass = window.gMkt.AssetD[Sym]
+            if(ass){
+                utils.copyTab(item, k)
+
+                let PrzMinIncSize = utils.getFloatSize(utils.getFullNum(ass.PrzMinInc || 6));
+                let VolMinValSize = utils.getFloatSize(ass.Mult || 2);
+
+                item.displaySym = utils.getSymDisplayName(window.gMkt.AssetD, item.Sym)
+                // 成交均价
+                item.Prz = Number(item.Prz || 0).toFixed2(PrzMinIncSize)
+                //成交数量
+                item.Sz = Number(item.Sz || 0).toFixed2(VolMinValSize)
+
+                item.PnlCls = Number(item.PnlCls || 0).toPrecision2(6,8)
+                item.Fee = Number(item.Fee || 0).toPrecision2(6,8)
+
+                item.Dir = item.Sz > 0?1:-1
+                item.DirStr = utils.getDirByStr(item.Dir)
+
+                item.AtStr = new Date(item.At).format('MM/dd hh:mm:ss'),
+
+                list.push(item)
+            }
+        }
+        list.sort(function(a,b){
+            return b.At - a.At
+        })
+        this.list = list
+    },
+    getTheadItem: function () {
+        return this.theadList.map(function (item, i) {
+            return m("th", { key: "historyOrdtHeadItem" + i, class: ""+item.class }, [
+                item.title
+            ])
+        })
+    },
+    getListItem: function () {
+        return this.list.map(function (item, i) {
+            return m("tr", { key: "historyOrdTableListItem" + i, class: "" }, [
+                
+                m("td", { class: "" }, [
+                    m("p", { class: " " }, [
+                        item.displaySym
+                    ])
+                ]),
+                m("td", { class: " " + utils.getColorStr(item.Dir, 'font') }, [
+                    item.DirStr
+                ]),
+                m("td", { class: " " }, [
+                    item.Prz
+                ]),
+                m("td", { class: " " }, [
+                    item.Sz
+                ]),
+                m("td", { class: " " }, [
+                    item.PnlCls
+                ]),
+                m("td", { class: " " }, [
+                    item.Fee
+                ]),
+                m("td", { class: "" }, [
+                    item.AtStr
+                ]),
+                m("td", { class: " " }, [
+                    item.PId.substr(-4)
+                ]),
+            ])
+        })
+    },
+}
+
 export default {
     oninit: function(vnode){
         
     },
     oncreate: function(vnode){
-        
+        obj.initEVBUS()
+        obj.getHistoryList()
+        obj.initObj()
     },
     view: function(vnode) {
         
-        return m("div",{class:"has-text-centered "},[
-            '历史成交'
+        return m("div", { class: "pub-history-trade table-container " }, [
+            m("table", { class: "table is-hoverable ", cellpadding: 0, cellspacing: 0 }, [
+                m("thead", { class: "" }, [
+                    m("tr", { class: "" }, [
+                        obj.getTheadItem()
+                    ])
+                ]),
+                m("tbody", { class: "" }, [
+                    obj.getListItem()
+                ])
+            ])
         ])
+    },
+    onremove: function(){
+        obj.rmEVBUS()
     }
 }
