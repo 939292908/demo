032ee78e74a9bdbd7aa925660e2493fbeacafbf9
HT <30796475+github-HT@users.noreply.github.com>
2020-04-24 17:32:08 +0800

    修改线路

--- src/reqConf/Conf.js
+++ src/reqConf/Conf.js
@@ -2,22 +2,22 @@
 export class CConf  {
     Active = {
         Name:"www.gmex.io",
-        WebAPI:"https://api-v1-01.gmex.io/www",
-        WSMKT:"wss://ws-v1-01.gmex.io/v1/market",
-        WSTRD:"wss://ws-v1-01.gmex.io/v1/trade"
+        WebAPI:"https://ss.abkjl.com/www",
+        WSMKT:"wss://ss.abkjl.com/v1/market",
+        WSTRD:"wss://ss.abkjl.com/v1/trade"
     }
     M = {
         "dev": {
             Name:"开发89环境",
-            WebAPI:"http://gs.gmex.io:8888",
-            WSMKT:"ws://gs.gmex.io:20080/v1/market",
-            WSTRD:"ws://gs.gmex.io:50301/v1/trade"
+            WebAPI:"http://gs.gmex.in:8888",
+            WSMKT:"ws://gs.gmex.in:20080/v1/market",
+            WSTRD:"ws://gs.gmex.in:50301/v1/trade"
         }
         ,"prod": {
             Name:"www.gmex.io",
-            WebAPI:"https://api-v1-01.gmex.io/www",
-            WSMKT:"wss://ws-v1-01.gmex.io/v1/market",
-            WSTRD:"wss://ws-v1-01.gmex.io/v1/trade"
+            WebAPI:"https://ss.abkjl.com/www",
+            WSMKT:"wss://ss.abkjl.com/v1/market",
+            WSTRD:"wss://ss.abkjl.com/v1/trade"
         }
     }
     GetActive() {
