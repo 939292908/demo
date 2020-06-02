2e20d2624c8fa4c7479ee4c4222d717f5099d380
HT <30796475+github-HT@users.noreply.github.com>
2020-06-02 20:08:47 +0800

    修改iOS浏览器交易价格输入框不能正常删除的bug

--- src/views/components/trade/stopPLMode.js
+++ src/views/components/trade/stopPLMode.js
@@ -152,8 +152,8 @@ let obj = {
     let lastNumbMin =  numb2.substr(numb2.length-1,1)
     //根据输入的是否含有“.”判断是否为小数
     if(e.target.value.includes(".")){
-        let beforValue = e.target.value.split(".")[0]? e.target.value.split(".")[0] : "0"
-        let eValue = e.target.value.split(".")[1] ? e.target.value.split(".")[1] : "0"
+        let beforValue = e.target.value.split(".")[0]? e.target.value.split(".")[0] : ""
+        let eValue = e.target.value.split(".")[1] ? e.target.value.split(".")[1] : ""
         //获取输入数字小数点后长度
         let eValueLength = eValue.length
         let lastValue = eValue.substr(eValue.length-1,1)
@@ -204,7 +204,7 @@ let obj = {
     //根据输入的是否含有“.”判断是否为小数
     if(e.target.value.includes(".")){
         let beforValue = e.target.value.split(".")[0]? e.target.value.split(".")[0] : "0"
-        let eValue = e.target.value.split(".")[1] ? e.target.value.split(".")[1] : "0"
+        let eValue = e.target.value.split(".")[1] ? e.target.value.split(".")[1] : ""
         //获取输入数字小数点后长度
         let eValueLength = eValue.length
         let lastValue = eValue.substr(eValue.length-1,1)
