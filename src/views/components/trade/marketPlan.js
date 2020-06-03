7926ce794ca9cc0c3e67b220e49d9af75a09c6ef
maoxs9257 <476377863@qq.com>
2020-06-03 15:18:21 +0800

    修改模式三止盈止损限制

--- src/views/components/trade/marketPlan.js
+++ src/views/components/trade/marketPlan.js
@@ -281,11 +281,14 @@ let obj = {
         let tradeType = window.$config.future.tradeType
         switch(tradeType){
             case 3:
-                if( dir == 1 && this.form.stopP <= this.form.stopL){
-                    return $message({title: gDI18n.$t('10193'/*'该仓位为多仓，止盈价需大于止损价'*/), content: gDI18n.$t('10193'/*'该仓位为多仓，止盈价需大于止损价'*/), type: 'danger'})
-                }else if(dir == -1 && this.form.stopP >= this.form.stopL){
-                    return $message({title: gDI18n.$t('10194'/*'该仓位为空仓，止盈价需小于止损价'*/), content: gDI18n.$t('10194'/*'该仓位为空仓，止盈价需小于止损价'*/), type: 'danger'})
+                if(this.form.stopP !="" && this.form.stopL !=""){
+                    if( dir == 1 && this.form.stopP <= this.form.stopL){
+                        return $message({title: gDI18n.$t('10193'/*'该仓位为多仓，止盈价需大于止损价'*/), content: gDI18n.$t('10193'/*'该仓位为多仓，止盈价需大于止损价'*/), type: 'danger'})
+                    }else if(dir == -1 && this.form.stopP >= this.form.stopL){
+                        return $message({title: gDI18n.$t('10194'/*'该仓位为空仓，止盈价需小于止损价'*/), content: gDI18n.$t('10194'/*'该仓位为空仓，止盈价需小于止损价'*/), type: 'danger'})
+                    }
                 }
+                
         }
         
         if(this.form.Prz === '0'){
