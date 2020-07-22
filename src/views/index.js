9ed64c62a8673c40cb0d4110b598be0b22c5e5f4
HT <haotong0119@outlook.com>
2020-07-22 21:03:31 +0800

    新增消息组件

--- src/views/index.js
+++ src/views/index.js
@@ -1,15 +1,26 @@
 let m = require('mithril')
 
 let header = require('@/views/header')
+let message = require('@/views/message')
 
 module.exports = {
-    oncreate: function(){
+    oninit: function(vnode) {
+        _console.log("ht","initialized")
+    },
+    oncreate: function(vnode) {
+        _console.log("ht","DOM created")
+    },
+    onupdate: function(vnode) {
+        _console.log("ht","DOM updated")
+    },
+    onremove: function(vnode) {
 
     },
     view: function(){
         return m('div.application.has-navbar-fixed-top', [
             m(header),
-            m('div.content')
+            m('div.content'),
+            m(message)
         ])
     }
 }
---
+++ src/views/message.js
@@ -0,0 +1,94 @@
+var m = require("mithril")
+
+import('@/styles/pages/message.css')
+
+let msg = {
+    DBG_MESSAGE: true,
+    DEL_INTERVAL: 10*1000,
+    messageContent: [],
+    initMsg: function(){
+        //全局message事件
+        let that = this
+        window.$message = function({title = '提示', content = '', type = 'dark'}){
+            if(that.DBG_MESSAGE){console.log(__filename,"MESSAGE",{title, content})}
+            that.addMessageDom({title,content,type})
+        }
+    },
+    addMessageDom: function (arg) {
+        let that = this;
+        let msg = this.createMsg(arg)
+        _console.log('ht', msg)
+        msg.delTimer = setTimeout(function () {
+            that.delMsg(msg.key)
+        }, that.DEL_INTERVAL)
+        this.messageContent.push(msg)
+        if (this.messageContent.length > 3) {
+            let needDelMsgArr = this.messageContent.slice(0, -3)
+            for (let i = 0; i < needDelMsgArr.length; i++) {
+                let item = needDelMsgArr[i]
+                this.delMsg(item.key)
+            }
+        }
+        m.redraw()
+    },
+    createMsg: function ({ title, content, type }) {
+        let that = this
+        let tm = Date.now()
+        if(window.isMobile){
+            return m('article', { class: "message box " + (' is-' + type), key: tm }, [
+                m('div', { class: "message-header" }, [
+                    m('p', { class: "" }, [
+                        content
+                    ]),
+                    m('button', {
+                        class: "delete", "aria-label": "delete", onclick: function () {
+                            that.delMsg(tm)
+                        }
+                    })
+                ])
+            ])
+        }else{
+            return m('article', { class: "message box " + (' is-' + type), key: tm }, [
+                m('div', { class: "message-header" }, [
+                    m('p', { class: "" }, [
+                        title
+                    ]),
+                    m('button', {
+                        class: "delete", "aria-label": "delete", onclick: function () {
+                            that.delMsg(tm)
+                        }
+                    })
+                ]),
+                m('div', { class: "message-body has-text-white" }, [
+                    content
+                ])
+            ])
+        }
+    },
+    delMsg: function (key) {
+        let i = this.messageContent.findIndex(item => {
+            return item.key == key
+        })
+        if (i > -1) {
+            if (this.messageContent[i].delTimer) {
+                clearTimeout(this.messageContent[i].delTimer)
+            }
+            this.messageContent.splice(i, 1)
+        }
+        m.redraw()
+    },
+}
+module.exports = {
+    oninit: function (vnode) {
+
+    },
+    oncreate: function (vnode) {
+        msg.initMsg()
+    },
+    view: function (vnode) {
+        return m('div', { class: "window-message-box " }, msg.messageContent)
+    },
+    onremove: function () {
+        
+    }
+}
