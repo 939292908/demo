3c9afd8712f15063aeec5aa69a927d31f4eefdd8
HT <haotong0119@outlook.com>
2020-07-22 21:03:39 +0800

    Update utils.js

--- src/util/utils.js
+++ src/util/utils.js
@@ -23,4 +23,30 @@ utils.removeItem = function (key) {
     }
 };
 
+utils.isMobile = function() {
+    let userAgentInfo = navigator.userAgent;
+ 
+    let mobileAgents = [ "Android", "iPhone", "SymbianOS", "Windows Phone", "iPad","iPod"];
+ 
+    let mobile_flag = false;
+ 
+    //根据userAgent判断是否是手机
+    for (let v = 0; v < mobileAgents.length; v++) {
+        if (userAgentInfo.indexOf(mobileAgents[v]) > 0) {
+            mobile_flag = true;
+            break;
+        }
+    }
+ 
+    let screen_width = window.screen.width;
+    let screen_height = window.screen.height;    
+ 
+    //根据屏幕分辨率判断是否是手机
+    if(screen_width < 500 && screen_height < 800){
+        mobile_flag = true;
+    }
+
+    return mobile_flag;
+}
+
 export default utils
