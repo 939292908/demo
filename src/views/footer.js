9e2ce5d56c3f48074e8eb177a9d71193e1cab5bc
HT <haotong0119@outlook.com>
2020-07-23 13:46:53 +0800

    Create footer.js

---
+++ src/views/footer.js
@@ -0,0 +1,36 @@
+let m = require('mithril')
+
+const methods = {
+    openNavbarDropdown: false,
+
+    clickNavbarOpenBtn: () => {
+        methods.openNavbarDropdown = !methods.openNavbarDropdown
+    }
+}
+
+module.exports = {
+    view:  function(){
+        return m('footer.footer', {}, [
+            m('div.content', {}, [
+                m('p.has-text-centered', {}, [
+                    m('strong', {}, [
+                        'Bulma'
+                    ]),
+                    ' by ',
+                    m('a', {href:""}, [
+                        'Jeremy Thomas'
+                    ]),
+                    '. The source code is licensed',
+                    m('a', {href:""}, [
+                        'MIT'
+                    ]),
+                    '. The website content is licensed ',
+                    m('a', {href:""}, [
+                        'CC BY NC SA 4.0'
+                    ]),
+                    '.'
+                ])
+            ])
+        ])
+    }
+}
