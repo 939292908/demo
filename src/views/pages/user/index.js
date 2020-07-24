9f3e75880ddc91382fc87ecc5f4dccde63bc07a9
HT <haotong0119@outlook.com>
2020-07-24 17:36:00 +0800

    Update index.js

--- src/views/pages/user/index.js
+++ src/views/pages/user/index.js
@@ -7,8 +7,7 @@ module.exports = {
     },
     view:  function(){
         return m('div.container', [
-            m('h1.title.is-1.has-text-title',['userCenter ']),
-            
+            m('h1.title.title-5.has-text-title',['userCenter ']),
         ])
     }
 }
