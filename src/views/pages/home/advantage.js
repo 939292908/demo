97ba00aaefab05c2304fd6bfcc6865793a2bca5a
HT <haotong0119@outlook.com>
2020-08-15 13:25:42 +0800

    Update advantage.js

--- src/views/pages/home/advantage.js
+++ src/views/pages/home/advantage.js
@@ -7,7 +7,7 @@ require('@/styles/pages/home.css');
 
 module.exports = {
     view: function () {
-        return m('div.views-pages-home-advantage', {}, [
+        return m('div.views-pages-home-advantage.theme--light', {}, [
             m('div', { class: `pages-home-advantage container` }, [
                 m('div', { class: `home-introduce is-around  w container py-5 border-1` }, [
                     // 平台优势
