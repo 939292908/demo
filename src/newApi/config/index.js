e272b2135f10961a66eed1270fbb38ada414d813
HT <haotong0119@outlook.com>
2020-08-24 16:03:30 +0800

    Update index.js

--- src/newApi/config/index.js
+++ src/newApi/config/index.js
@@ -8,8 +8,19 @@ instConf.updateNetLines();
 const api = instConf.GetActive();
 const Http = new Axios(api.WebAPI).service;
 
+const Interval = 1000;
+const WSApi = require('@/api/wsApi');
+gWsApi = new WSApi({
+    baseUrl: api.WSMKT,
+    Typ: 'mkt'
+});
+setInterval(function () {
+    gWsApi.stately.do(gWsApi);
+}, Interval);
+
 export default {
     api,
     Conf,
-    Http
+    Http,
+    gWsApi
 };
