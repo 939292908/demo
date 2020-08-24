b6b52d5fa37145a1afd4cce653d7c3033cdad7c3
HaoHaoP <HaoHaoP@HaoHaoP.com>
2020-08-24 20:40:01 +0800

    忘记密码

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,11 @@ m.route(document.querySelector('body .route-box'), defaultRoutePath, {
             return import('@/pages/page/login/register/registerView');
         }
     },
+    '/forgetPassword': {
+        onmatch: function () {
+            return import('@/pages/page/login/forgetPassword/forgetPasswordView');
+        }
+    },
     // '/myWalletIndex': {
     //     onmatch: function () {
     //         return import('@/views/pages/Myassets/myWalletIndex');
