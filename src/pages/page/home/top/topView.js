5c73d29f886324db9e0c4078398bf81ab5939b11
CQQ <15073404489@189.cn>
2020-08-25 16:16:03 +0800

    首页尺寸改动 导航栏样式改动

--- src/pages/page/home/top/topView.js
+++ src/pages/page/home/top/topView.js
@@ -5,14 +5,14 @@ require('@/styles/pages/home/top.scss');
 
 module.exports = function (props) {
     const { banneList, noticeList } = props.data;
-    return m('div.views-pages-home-top', {
+    return m('div.views-pages-home-top has-bg-sub-level-1 ', {
     }, [
         // 顶部
-        m('div', { class: `home-banner has-text-centered`, style: `background: url(${require("@/assets/img/home/vector.png").default}) no-repeat center center / 100% 100%;` }, [
+        m('div', { class: ` has-text-centered `, style: `background: url(${require("@/assets/img/home/vector.png").default}) no-repeat center center / 100% 100%;` }, [
             m('p', { class: `font-weight-regular pt-8 title-x-large-1` }, ['最值得信任的数字资产交易平台']),
             m('p', { class: `mt-5 title-small font-weight-regular has-text-level-2` }, ['自主研发钱包加密技术，全面保护用户数字资产安全']),
-            m('a', { class: `purchase-btn mt-7 theme--light has-bg-primary btn-2 button title-medium font-weight-regular`, onclick: this.toPage, target: "_blank", href: "http://localhost:8080/#!/register" }, ['立即交易']),
-            m('div', { class: `has-text-centered mt-5` }, [
+            m('a', { class: `purchase-btn mt-8 theme--light has-bg-primary btn-2 button title-medium font-weight-regular`, onclick: this.toPage, target: "_blank", href: "http://localhost:8080/#!/register" }, ['立即交易']),
+            m('div', { class: `has-text-centered mt-4` }, [
                 m('a', { class: `has-text-level-2`, href: "http://localhost:8080/#!/register", target: "_blank" }, ['还没账号？立即前往账号注册 →'])
             ]),
             m('div', { class: `top-bottom-box mt-8 container` }, [
