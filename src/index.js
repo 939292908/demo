2d86535274ee889d0b75ce33cad7a3a7c375bcca
leezCtt <leez1212@163.com>
2020-08-25 15:52:42 +0800

    # 全新的api

--- src/index.js
+++ src/index.js
@@ -18,8 +18,10 @@ import('swiper/swiper-bundle.css');
 import('./libs/gt');
 
 // 重写console.log
-require('@/util/log');
+// require('@/util/log');
 
+const leez = require('./newApi2');
+console.log(leez);
 window.onresize = function (arg) {
     // 判断是否是移动端
     broadcast.emit({ cmd: 'ONRESIZE_UPD', data: { Ev: 'ONRESIZE_UPD' } });
