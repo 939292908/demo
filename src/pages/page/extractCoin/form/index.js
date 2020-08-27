65182dcf208c86488e8a6421604eb868bbe277ec
leezCtt <leez1212@163.com>
2020-08-27 20:58:50 +0800

    # 提币

--- src/pages/page/extractCoin/form/index.js
+++ src/pages/page/extractCoin/form/index.js
@@ -9,52 +9,55 @@ module.exports = {
             m('div.form-block', [
                 m('div.formModule', [
                     m('div.label has-text-title body-5', '币种'),
-                    m('div.control', [
+                    m('div.control has-icons-right', [
                         m('div.select is-fullwidth', m('select', [
                             m('option', { selected: true }, 'Country1'),
                             m('option', { selected: false }, 'Country2'),
                             m('option', { selected: false }, 'Country3')
-                        ]))
+                        ])),
+                        m('span.icon is-small is-right', 'wefw')
                     ])
                 ]),
                 m('div.formModule', [
                     m('div.label has-text-title body-5', [
                         m('span', '标签'),
-                        m('i')
+                        m('i.iconfont.icon-Personal')
                     ]),
-                    m('div.control', [
-                        m('input.input', { type: 'text', placeholder: 'Normal input' })
+                    m('div.control line-label', [
+                        m('input.input body-5', { type: 'text', placeholder: 'Normal input', value: '121' })
                     ])
                 ]),
                 m('div.formModule', [
                     m('div.label has-text-title body-5', [
                         m('span', '链名称'),
-                        m('i')
+                        m('i.iconfont.icon-Personal')
                     ]),
-                    m('div.control', [
-                        m('button.button', 'JOGGUU'),
-                        m('button.button is-info', 'JOGGUU'),
-                        m('button.button', 'JOGGUU')
+                    m('div.control dis-flex', [
+                        m('div.butItem', m('div', 'JOGGUU')),
+                        m('div.butItem butItemActive', m('div', 'JOGGUU')),
+                        m('div.butItem', m('div', 'JOGGUU'))
                     ])
                 ]),
                 m('div.formModule', [
                     m('div.label has-text-title body-5', '提币地址'),
-                    m('div.control', [
-                        m('input.input', { type: 'text', placeholder: 'Normal input' })
-                    ])
+                    m('div.control address', [
+                        m('input.input body-5', { type: 'text', placeholder: 'Normal input', value: '90909090' })
+                    ]),
+                    m('div.errorToTal body-4', '地址错误')
                 ]),
                 m('div.formModule', [
                     m('div.label has-text-title body-5', [
                         m('span', '数量'),
                         m('i')
                     ]),
-                    m('div.control', [
-                        m('input.input', { type: 'text', placeholder: 'Normal input' })
+                    m('div.control extract-num', [
+                        m('input.input body-5', { type: 'number', placeholder: 'Normal input' })
                     ]),
-                    m('div', [
+                    m('div.dis-flex item-space charge body-4', [
                         m('div', `可提：0BTC`),
                         m('div', `手续费：0.001BTC`)
-                    ])
+                    ]),
+                    m('div.errorToTal body-4', '数量错误')
                 ]),
                 m('button.button is-info is-fullwidth', '确定')
             ]),
