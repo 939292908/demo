7c5273048cef7810c3360f83644003fda15bc5e5
leezCtt <leez1212@163.com>
2020-08-27 10:48:04 +0800

    # 修改 行情

---
+++ src/pages/page/extractCoin/index.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-26 14:43:08
+ * @LastEditTime: 2020-08-26 17:54:10
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \website-project\src\pages\page\extractCoin\index.js
+ */
+const m = require("mithril");
+
+module.exports = {
+    view: function () {
+        return m('div', 'woqbdqwkwdnq');
+    }
+};
