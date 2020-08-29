d5931fc1275a9385b8154117e0b453237fee49ca
NZM <nizimei0@163.com>
2020-08-29 11:25:22 +0800

    充币

--- src/views/page/myAssets/myWalletIndex/children/recharge/index.js
+++ src/views/page/myAssets/myWalletIndex/children/recharge/index.js
@@ -2,24 +2,24 @@ const wlt = require('@/models/wlt/wlt');
 const Http = require('@/api').webApi;
 
 module.exports = {
-    selData: [],
+    pageData: [],
     tips: '*您只能向此地址充值BTC，其他资产充入BTC地址将无法找回 *使用BTC地址充值需要1个网络确认才能到账 *默认充入我的钱包，您可以通过“资金划转”将资金转至交易账户或者其他账户',
-    setSelData: function(ary) {
-        for (const i in ary) {
-            this.selData.push(ary[i].wType);
-            Http.GetRechargeAddr({
-                wType: ary[i].wType
-            }).then(function(arg) {
-                console.log('nzm', 'GetRechargeAddr success', arg);
-            }).catch(function(err) {
-                console.log('nzm', 'GetRechargeAddr error', err);
-            });
-        }
+    setSelData: function(arg) {
+        console.log(arg);
     },
     initFn: function () {
         wlt.init();
         setTimeout(() => {
-            this.setSelData(wlt.wallet['03']);
+            for (const i in wlt.wallet['03']) {
+                // this.selData.push(wlt.wallet['03'][i].wType);
+                Http.GetRechargeAddr({
+                    wType: wlt.wallet['03'][i].wType
+                }).then(function(arg) {
+                    console.log('nzm', 'GetRechargeAddr success', arg);
+                }).catch(function(err) {
+                    console.log('nzm', 'GetRechargeAddr error', err);
+                });
+            }
         }, 200);
     },
     updateFn: function (vnode) {
