2fa97411b75b501113b2ea5f80f841854af1ca9a
HaoHaoP <HaoHaoP@HaoHaoP.com>
2020-08-29 18:10:06 +0800

    样式

--- src/views/page/myAssets/walletTable/walletTable.view.js
+++ src/views/page/myAssets/walletTable/walletTable.view.js
@@ -2,7 +2,7 @@ const m = require('mithril');
 const commonSelectionBox = require('../commonSelectionBox/commonSelectionBox.view');
 
 module.exports = {
-    view (vnode) {
+    view(vnode) {
         return m('div', { class: 'views-pages-Myassets-assetRecords-assetRecordsWallet mt-7 px-4' }, [
             m(commonSelectionBox, { num: '03' }),
             m('div', {}, [
@@ -10,58 +10,55 @@ module.exports = {
                     m('tbody', { class: (vnode.attrs.datadisplayvalue === 1 ? '' : 'datadisplay') + ' tbody' }, [
                         vnode.attrs.dataArrObj.map(items => {
                             return m('tr', { class: 'has-text-level-2 body-4 pb-3' }, [
-                                m('td', {}, [items.category]),
-                                m('td.px-8 pb-4', {}, [items.type]),
-                                m('td.px-8 pb-4', {}, [items.num]),
-                                m('td.px-7 pb-4', {}, [items.ServiceCharge]),
-                                m('td.px-7 pb-4', {}, [items.state]),
-                                m('td.px-8 pb-4', {}, [items.time]),
-                                m('td', { class: 'tbodytd' }, [items.remarks])
+                                m('td.py-4', {}, [items.category]),
+                                m('td.px-8.py-4', {}, [items.type]),
+                                m('td.px-8.py-4', {}, [items.num]),
+                                m('td.px-8.py-4', {}, [items.ServiceCharge]),
+                                m('td.px-8.py-4', {}, [items.state]),
+                                m('td.px-8.py-4', {}, [items.time]),
+                                m('td.pl-8.py-4', { class: 'tbodytd' }, [items.remarks])
                             ]);
                         }),
                         vnode.attrs.grossValue.map((item, index) => {
-                            return m('tr', { class: 'body-4' }, [
-                                m('td', {}, [item.wType]),
-                                m('td.px-8 pb-7', {}, [item.status]),
-                                m('td.px-8 pb-7', {}, [item.num]),
-                                m('td.px-7 pb-7', {}, [item.num]),
-                                m('td.px-7 pb-7', {}, [item.stat]),
-                                m('td.px-8 pb-7', {}, [item.time]),
-                                m('td', { class: 'has-text-primary cursor-pointer tbodytd' }, [
-                                    m('div', { class: 'dropdown is-right is-active' }, [
-                                        m('div', {
-                                            class: 'dropdown-trigger',
-                                            onclick: function () {
-                                                vnode.attrs.displayEvnet(index);
-                                            }
-                                        }, [
-                                            m('span', { ariaHaspopup: 'true', ariaControls: 'dropdown-menu6' }, ['详情']),
-                                            m('span', { class: '' }, [
-                                                m('i', { class: 'iconfont ' + (vnode.attrs.displayValue === index && vnode.attrs.noDisplay ? 'icon-xiala' : 'icon-xiala'), ariaHidden: 'true' })
+                            return [
+                                m('tr', { class: 'body-4' }, [
+                                    m('td.py-4', {}, [item.wType]),
+                                    m('td.px-8.py-4', {}, [item.status]),
+                                    m('td.px-8.py-4', {}, [item.num]),
+                                    m('td.px-7.py-4', {}, [item.num]),
+                                    m('td.px-7.py-4', {}, [item.stat]),
+                                    m('td.px-8.py-4', {}, [item.time]),
+                                    m('td.pl-8.py-4.has-text-primary', {
+                                        onclick: function() {
+                                            vnode.attrs.displayEvnet(index);
+                                        }
+                                    }, [
+                                        m('span', { ariaHaspopup: 'true', ariaControls: 'dropdown-menu6' }, ['详情']),
+                                        m('i.iconfont', {
+                                            class: vnode.attrs.displayValue === index && vnode.attrs.noDisplay ? 'icon-xiala' : 'icon-xiala'
+                                        }, [])
+                                    ])
+                                ]),
+                                m('tr.has-bg-level-3', { hidden: !(vnode.attrs.displayValue === index && vnode.attrs.noDisplay) }, [
+                                    m('td.px-5.body-4.has-text-level-3', { colspan: 7 }, [
+                                        m('div.my-3.columns', {}, [
+                                            m('div.column', {}, ['提币地址：']),
+                                            m('div.column', {}, ['链类型：',
+                                                m('span.has-text-level-1', {}, [item.wType])
                                             ])
                                         ]),
-                                        m('div', { class: vnode.attrs.displayValue === index && vnode.attrs.noDisplay ? 'dropdown-menu' : 'dropdown-menu1', id: 'dropdown-menu6', role: 'menu' }, [
-                                            m('div', { class: 'dropdown-content', style: 'width:1170px' }, [
-                                                m('div', { class: 'dropdown-item' }, [
-                                                    m('div', { class: '' }, [
-                                                        m('div', { class: 'mb-3' }, [
-                                                            m('span', { class: 'pr-7' }, ['提币地址:']),
-                                                            m('span', {}, ['链类型:' + item.wType])
-                                                        ]),
-                                                        m('div', {}, [
-                                                            m('span', { class: 'pr-7' }, ['提币地址:']),
-                                                            m('span', {}, ['链类型:' + item.wType])
-                                                        ])
-                                                    ])
-                                                ])
+                                        m('div.columns.mb-3', {}, [
+                                            m('div.column', {}, ['提币地址：']),
+                                            m('div.column', {}, ['链类型：',
+                                                m('span.has-text-level-1', {}, [item.wType])
                                             ])
                                         ])
                                     ])
                                 ])
-                            ]);
-                        })
-                    ]),
-                    m('div', { class: vnode.attrs.datadisplayvalue === 0 ? 'disdatadisplay' : 'datadisplay' + ' ' }, ['暂无数据'])
+                            ];
+                        }),
+                        m('div', { class: vnode.attrs.datadisplayvalue === 0 ? 'disdatadisplay' : 'datadisplay' + ' ' }, ['暂无数据'])
+                    ])
                 ])
             ])
         ]);
