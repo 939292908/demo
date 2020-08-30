91f85a5ace4991c3667d47cad03db828404c1ffb
NZM <nizimei0@163.com>
2020-08-30 16:38:36 +0800

    充币 NT

--- src/views/page/myAssets/myWalletIndex/children/recharge/rechargeView.js
+++ src/views/page/myAssets/myWalletIndex/children/recharge/rechargeView.js
@@ -3,10 +3,9 @@ const rechargeIndex = require('@/views/page/myAssets/myWalletIndex/children/rech
 require('@/views/page/myAssets/myWalletIndex/children/recharge/recharge.scss');
 
 module.exports = {
-    pageData: [],
-    tips: '*您只能向此地址充值BTC，其他资产充入BTC地址将无法找回 *使用BTC地址充值需要1个网络确认才能到账 *默认充入我的钱包，您可以通过“资金划转”将资金转至交易账户或者其他账户',
     oninit: () => {
         rechargeIndex.initFn();
+        m.redraw();
     },
     oncreate: () => {
     },
@@ -30,19 +29,45 @@ module.exports = {
                             ])
                         )
                     ]),
+                    m('div', { class: `xrpLable mb-7`, style: { display: rechargeIndex.selectCheck === 'XRP' ? '' : 'none' } }, [
+                        m('span', {}, '标签'),
+                        m('div', { class: `mt-2 px-2 has-text-primary border-radius-small` }, rechargeIndex.uId)
+                    ]),
+                    m('div', { class: `usdtLable mb-7`, style: { display: rechargeIndex.selectCheck === 'USDT' ? '' : 'none' } }, [
+                        m('span', {}, '标签'),
+                        m('div', { class: `mt-2` }, [
+                            rechargeIndex.USDTLabel.map((item, index) => {
+                                return m('button', {
+                                    class: `mr-6 cursor-pointer ` + (rechargeIndex.btnCheckFlag === index ? `has-bg-primary` : `noneBG`),
+                                    onclick: () => { rechargeIndex.changeBtnflag(index); }
+                                }, item.title);
+                            })
+                        ])
+                    ]),
                     m('div', {}, [
                         m('span', { class: `body-5` }, '充币地址')
                     ]),
                     m('div', { class: `currencyAddr border-radius-medium mt-2 mb-7` }, [
                         m('div', { class: `currencyAddr-text ml-3` }, [
-                            m('input', { class: `addrText`, type: 'text', readOnly: `readOnly`, value: `1323232` })
+                            m('input', { class: `addrText`, type: 'text', readOnly: `readOnly`, value: rechargeIndex.rechargeAddr })
                         ]),
                         m('div', { class: `currencyAddr-Operation ml-3` }, [
-                            m('i', { class: `iconfont icon-copy has-text-primary cursor-pointer`, onclick: () => { rechargeIndex.copyText(); } }),
-                            m('i', { class: `iconfont icon-QrCode has-text-primary` })
+                            m('div', { class: `iImg mt-2` }, [
+                                m('i', { class: `iconfont icon-copy has-text-primary cursor-pointer`, onclick: () => { rechargeIndex.copyText(); } }),
+                                m('i', {
+                                    class: `iconfont icon-QrCode has-text-primary cursor-pointer`,
+                                    onmouseover: () => {
+                                        rechargeIndex.changeQrcodeDisplay('show');
+                                    },
+                                    onmouseout: () => {
+                                        rechargeIndex.changeQrcodeDisplay('hide');
+                                    }
+                                })
+                            ]),
+                            m('div', { class: `QrCodeImg`, style: { display: rechargeIndex.qrcodeDisplayFlag ? '' : 'none' } })
                         ])
                     ]),
-                    m('div', {}, [
+                    m('div', { class: `tips` }, [
                         m('span', {}, '温馨提示'),
                         m('br'),
                         rechargeIndex.tips.split('*').map(item => m('span', {}, '*' + item))
@@ -55,7 +80,6 @@ module.exports = {
                     ]),
                     m('hr')
                 ])
-
             ])
         ]);
     },
