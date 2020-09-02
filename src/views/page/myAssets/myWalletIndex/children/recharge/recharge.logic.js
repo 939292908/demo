38fc6d5b912d82eb2f86ebe48fcf6a2df57fb37c
NZM <nizimei0@163.com>
2020-09-02 22:34:58 +0800

    添加多语言

--- src/views/page/myAssets/myWalletIndex/children/recharge/recharge.logic.js
+++ src/views/page/myAssets/myWalletIndex/children/recharge/recharge.logic.js
@@ -4,6 +4,7 @@ const m = require('mithril');
 const wlt = require('@/models/wlt/wlt');
 const broadcast = require('@/broadcast/broadcast');
 const Qrcode = require('qrcode');
+const l180n = require('@/languages/I18n').default;
 
 module.exports = {
     pageData: [], // 所需数据
@@ -95,9 +96,18 @@ module.exports = {
             if (this.pageData[i].wType === this.form.selectCheck) {
                 const networkNum = this.pageData[i].networkNum;
                 if (this.pageData[i].promptRecharge) {
-                    this.tips = this.pageData[i].promptRecharge + '*您只能向此地址充值' + this.form.selectCheck + '，其他资产充入' + this.form.selectCheck + '地址将无法找回 *使用' + this.form.selectCheck + '地址充值需要' + networkNum + '个网络确认才能到账 *默认充入我的钱包，您可以通过“资金划转”将资金转至交易账户或者其他账户';
+                    this.tips = this.pageData[i].promptRecharge +
+                    '*您只能向此地址充值' + this.form.selectCheck + '，其他资产充入' + this.form.selectCheck + '地址将无法找回' +
+                    /* 使用{value1}地址充币需要{value2}个网络确认才能到账 */
+                    '*' + l180n.$t('10084', { value1: this.form.selectCheck, value2: networkNum }) +
+                    /* '默认充入我的钱包，您可以通过“资金划转”将资金转至交易账户或者其他账户' */
+                    '*' + l180n.$t('10085');
                 } else {
-                    this.tips = '您只能向此地址充值' + this.form.selectCheck + '，其他资产充入' + this.form.selectCheck + '地址将无法找回 *使用' + this.form.selectCheck + '地址充值需要' + networkNum + '个网络确认才能到账 *默认充入我的钱包，您可以通过“资金划转”将资金转至交易账户或者其他账户';
+                    this.tips = '您只能向此地址充值' + this.form.selectCheck + '，其他资产充入' + this.form.selectCheck + '地址将无法找回*' +
+                    /* 使用{value1}地址充币需要{value2}个网络确认才能到账 */
+                    '*' + l180n.$t('10084', { value1: this.form.selectCheck, value2: networkNum }) +
+                    /* '默认充入我的钱包，您可以通过“资金划转”将资金转至交易账户或者其他账户' */
+                    '*' + l180n.$t('10085');
                 }
                 this.memo = this.pageData[i].memo;
                 this.openChains = this.pageData[i].openChains;
