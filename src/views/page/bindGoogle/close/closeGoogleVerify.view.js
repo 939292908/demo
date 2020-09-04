bd071a7811c5878e608ccd4c73095429333752eb
NZM <nizimei0@163.com>
2020-09-04 21:14:48 +0800

    添加多语言

--- src/views/page/bindGoogle/close/closeGoogleVerify.view.js
+++ src/views/page/bindGoogle/close/closeGoogleVerify.view.js
@@ -1,7 +1,7 @@
 const m = require('mithril');
 require('@/views/page/bindGoogle/bindGoogle.scss');
 const closeGLogic = require('@/views/page/bindGoogle/close/closeGoogleVerify.logic');
-const l180n = require('@/languages/I18n').default;
+const I18n = require('@/languages/I18n').default;
 
 const closeGView = {
     oninit: () => {
@@ -11,12 +11,12 @@ const closeGView = {
         return m('div', { class: `views-page-accountSecurity-bindGoogle-close theme--light pb-7` }, [
             m('div', { class: `operation mb-7 has-bg-level-2` }, [
                 m('i', { class: `iconfont icon-Return has-text-title mr-7` }),
-                m('span', { class: `has-text-title my-4 ml-4 title-medium` }, l180n.$t('10262') /* '您正在解绑谷歌验证' */)
+                m('span', { class: `has-text-title my-4 ml-4 title-medium` }, I18n.$t('10262') /* '您正在解绑谷歌验证' */)
             ]),
             m('div', { class: `center content-width` }, [
                 m('div', { class: `warning mb-3 pl-7` }, [
                     m('i', { class: `iconfont icon-Tooltip pr-2 has-text-primary` }),
-                    m('span', { class: `has-text-level-3` }, l180n.$t('10263') /* '出于安全考虑，修改账户安全项之后，24h内禁止提币' */)
+                    m('span', { class: `has-text-level-3` }, I18n.$t('10263') /* '出于安全考虑，修改账户安全项之后，24h内禁止提币' */)
                 ]),
                 m('div', { class: `closeOperation pt-8 has-bg-level-2` }, [
                     m('div', { class: `pwdDiv` }, [
@@ -25,7 +25,7 @@ const closeGView = {
                         m('input', { class: `border-radius-small mb-5 mt-2 pwd`, type: `text` })
                     ]),
                     m('div', { class: `codeDiv` }, [
-                        m('span', { class: `body-5 mb-2` }, l180n.$t('10264') /* '原谷歌验证码' */),
+                        m('span', { class: `body-5 mb-2` }, I18n.$t('10264') /* '原谷歌验证码' */),
                         m('br'),
                         m('input', { class: `border-radius-small mt-2 code`, type: `text` })
                     ]),
