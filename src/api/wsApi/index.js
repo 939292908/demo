74bbe701865a0741888652fb64cfc06fb008fa73
HT8080 <haotong0119@outlook.com>
2020-09-05 20:12:09 +0800

    修改行情链接vp选择

--- src/api/wsApi/index.js
+++ src/api/wsApi/index.js
@@ -1,10 +1,12 @@
 const ActiveLine = require('../config').ActiveLine;
 const WSApi = require('./Mkt');
+const config = require('../../config.js');
 
 const Interval = process.env.BUILD_ENV === 'prod' ? 50 : 1000;
 const gWsApi = new WSApi({
     baseUrl: ActiveLine.WSMKT,
-    Typ: 'mkt'
+    Typ: 'mkt',
+    vp: config.exchId
 });
 setInterval(function () {
     gWsApi.stately.do(gWsApi);
