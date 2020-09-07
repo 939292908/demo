66eb1b8d92fb524359a06b971bb3d7d064c91a65
939292908 <64248504+939292908@users.noreply.github.com>
2020-09-07 16:49:23 +0800

    newDropdown下拉

--- src/views/components/common/DropdownNew/Dropdown.logic.js
+++ src/views/components/common/DropdownNew/Dropdown.logic.js
@@ -13,113 +13,37 @@
 // btnWidth 按钮 宽 (选填)
 // btnHeight 按钮 高 (选填)
 // menuWidth 菜单 宽 (选填)
-
-class Dropdown {
-    constructor () {
-        this.showMenu = false;
-        this.curItem = {};
-        this.curId = "";
-        this.menuList = [
-            {
-                id: 1,
-                label: "111"
-            },
-            {
-                id: 2,
-                label: "222"
+const broadcast = require('@/broadcast/broadcast');
+module.exports = {
+    // 初始化 全局广播
+    initEVBUS (vnode) {
+        // 订阅 body点击事件广播
+        broadcast.onMsg({
+            key: vnode.attrs.evenKey,
+            cmd: broadcast.EV_ClICKBODY,
+            cb: function () {
+                vnode.attrs.setOption({
+                    showMenu: false
+                });
             }
-        ];
-        this.oninit = vnode => {
-            // this.initCurItem(vnode);
-            // this.initEVBUS(vnode);
-        };
-
-        this.oncreate = vnode => {
-        };
-
-        this.onupdate = vnode => {
-            // this.initCurItem(vnode);
-        };
-
-        this.onremove = vnode => {
-            // this.rmEVBUS(vnode);
-        };
+        });
+    },
+    // 删除全局广播
+    rmEVBUS (vnode) {
+        // 删除 body点击事件广播
+        broadcast.offMsg({
+            key: vnode.attrs.evenKey,
+            isall: true
+        });
+    },
+    oninit(vnode) {
+        this.initEVBUS(vnode);
+    },
+    oncreate(vnode) {
+    },
+    onupdate(vnode) {
+    },
+    onremove(vnode) {
+        this.rmEVBUS(vnode);
     }
-
-    setShowMenu (type) {
-        this.showMenu = type;
-        console.log(type);
-    }
-
-    menuClick () {
-        console.log(this.curItem);
-    }
-}
-
-module.exports = new Dropdown();
-
-// module.exports = {
-//     // ============= 状态 =============
-//     showMenu: false,
-//     curItem: {},
-//     curId: "", // 临时保存id
-//     openClickBody: true, // body事件 节流
-
-//     // ============= 方法 =============
-//     // 初始化 选中item
-//     initCurItem (vnode) {
-//         // 临时保存 当前选中id
-//         vnode.attrs.activeId && vnode.attrs.activeId((p, k) => {
-//             return (vnode.state.curId = p[k]);
-//         });
-//         // 当前选中item
-//         const item = vnode.attrs.getList().find(item => {
-//             return item.id === vnode.state.curId;
-//         });
-//         vnode.state.curItem = item || {};
-
-//         // console.log(vnode.state.curId, vnode.state.curItem, 66);
-//         // if (curItem) {
-//         //     vnode.state.btnText = curItem.label;
-//         // } else {
-//         //     vnode.state.btnText = vnode.attrs.placeholder || "--";
-//         // }
-//         // console.log(curItem, vnode.state.btnText, 77777777777);
-//         m.redraw();
-//     },
-//     // 初始化 全局广播
-//     initEVBUS (vnode) {
-//         // 订阅 body点击事件广播
-//         broadcast.onMsg({
-//             key: vnode.attrs.evenKey,
-//             cmd: broadcast.EV_ClICKBODY,
-//             cb: function () {
-//                 vnode.attrs.setShowMenu(false);
-//             }
-//         });
-//         // this.EV_ClICKBODY_unbinder = window.gEVBUS.on(gEVBUS.EV_ClICKBODY, arg => {
-//         //     if (vnode.state.openClickBody) vnode.attrs.showMenu((p, k) => p[k] = false) // body事件 节流
-//         //     vnode.state.openClickBody = true
-//         // })
-//     },
-//     // 删除全局广播
-//     rmEVBUS (vnode) {
-//         broadcast.offMsg({
-//             key: vnode.attrs.evenKey,
-//             isall: true
-//         }); // 删除 body点击事件广播
-//     },
-//     // ============= 生命周期 =============
-//     oninit (vnode) {
-//         this.initCurItem(vnode);
-//         this.initEVBUS(vnode);
-//     },
-//     oncreate (vnode) {
-//     },
-//     onupdate (vnode) {
-//         this.initCurItem(vnode);
-//     },
-//     onremove: function (vnode) {
-//         this.rmEVBUS(vnode);
-//     }
-// };
+};
--- src/views/components/common/DropdownNew/Dropdown.view.js
+++ src/views/components/common/DropdownNew/Dropdown.view.js
@@ -1,65 +1,70 @@
 // 参数说明
-// getList() {return [{ id:xxx, label:xxx,... }, { id:xxx, label:xxx,... }]} 菜单数据 (id, label必须项) (必填)
 // evenKey body事件key (必填)
+// currentId 选中id (必填)
+// showMenu 下拉状态 (必填)
+// setOption (设置currentId和showMenu) (必填)
+// menuList() {return [{ id:xxx, label:xxx,... }, { id:xxx, label:xxx,... }]} 菜单数据 (id, label) (必填) (render(){} 自定义内容)
 
 // onClick(item) {} 点击事件 可获取item (选填)
-// activeId 默认选中id(fn) (必填)
-// type 触发类型：active / hover (选填)默认active
-
-// placeholder 提示文字 (选填)
 // class 类名 (选填)
+// placeholder 提示文字 (选填)
 // btnClass 按钮 类名 (选填)
 
 // btnWidth 按钮 宽 (选填)
 // btnHeight 按钮 高 (选填)
 // menuWidth 菜单 宽 (选填)
 
+// type 触发类型：active / hover (选填)(默认active)
+// showMenuIcon 菜单 icon (选填)(默认false)
+
 var m = require("mithril");
 require('./Dropdown.scss');
-const model = require('./Dropdown.logic.js');
-console.log(model.menuList);
+const logic = require('./Dropdown.logic.js');
+
 module.exports = {
-    oninit: vnode => model.oninit(vnode),
-    oncreate: vnode => model.oncreate(vnode),
-    onupdate: vnode => model.onupdate(vnode),
-    onremove: vnode => model.onremove(vnode),
+    oninit: vnode => logic.oninit(vnode),
+    oncreate: vnode => logic.oncreate(vnode),
+    onupdate: vnode => logic.onupdate(vnode),
+    onremove: vnode => logic.onremove(vnode),
     view (vnode) {
-        return m('div', { class: `${model.class || ''} my-dropdown dropdown ${model.type === 'hover' ? " is-hoverable" : model.showMenu ? " is-active" : ''}` }, [
+        return m('div', { class: `${vnode.attrs.class || ''} my-dropdown dropdown ${vnode.attrs.type === 'hover' ? " is-hoverable" : vnode.attrs.showMenu ? " is-active" : ''}` }, [
             // btn
             m('div', { class: "dropdown-trigger has-text-1" }, [
                 m('button', {
-                    class: `button ${model.btnClass || ''}`,
-                    style: (model.btnWidth ? `width:${model.btnWidth}px;` : '') +
-                        (model.btnHeight ? `height:${model.btnHeight}px;` : ''),
-                    onclick: (e) => {
-                        setTimeout(() => model.setShowMenu(!model.showMenu), 0); // 进入下一次事件队列，先让body事件关闭所有下拉，再开启自己
-                        // model.setBodyEven(false)
-                        // window.stopBubble(e)
+                    class: `button ${vnode.attrs.btnClass || ''}`,
+                    style: (vnode.attrs.btnWidth ? `width:${vnode.attrs.btnWidth}px;` : '') +
+                        (vnode.attrs.btnHeight ? `height:${vnode.attrs.btnHeight}px;` : ''),
+                    onclick: e => {
+                        // 进入下一次事件队列，先让body事件关闭所有下拉，再开启自己
+                        const type = vnode.attrs.showMenu;
+                        setTimeout(() => {
+                            vnode.attrs.setOption({ showMenu: !type });
+                        }, 0);
                     }
                 }, [
-                    m('p', { class: `my-trigger-text` }, model.curItem.label), // btnText
-                    m('i', { class: "my-trigger-icon iconfont icon-xiala has-text-primary" }) // icon
+                    vnode.attrs.menuList().find(item => item.id === vnode.attrs.currentId)?.render
+                        ? vnode.attrs.menuList().find(item => item.id === vnode.attrs.currentId)?.render()
+                        : vnode.attrs.menuList().find(item => item.id === vnode.attrs.currentId)?.label,
+                    m('i', { class: `my-trigger-icon iconfont icon-xiala has-text-primary` })
                 ])
             ]),
             // menu
-            m('div', { class: "dropdown-menu ", style: model.menuWidth ? `width:${model.menuWidth}px` : '' }, [
+            m('div', { class: "dropdown-menu ", style: vnode.attrs.menuWidth ? `width:${vnode.attrs.menuWidth}px` : '' }, [
                 m('div', { class: "dropdown-content", style: "max-height: 400px; overflow: auto;" },
-                    model.menuList.map((item, index) => {
+                    vnode.attrs.menuList().map((item, index) => {
                         return m('a', {
-                            class: `dropdown-item has-hover ${model.curId === item.id ? 'has-active' : ''}`,
-                            key: item.label + index,
+                            class: `dropdown-item has-hover ${vnode.attrs.currentId === item.id ? 'has-active' : ''}`,
+                            key: item.id + index,
                             onclick () {
-                                model.curItem = item; // 同步显示文字
-                                console.log(model.curItem, item.label);
-                                model.activeId((p, k) => {
-                                    return (p[k] = item.id);
-                                }); // 修改选中id
-                                model.onClick && model.onClick(item); // 传递数据
-                                model.setShowMenu(false); // 关闭菜单
+                                vnode.attrs.onClick && vnode.attrs.onClick(item); // 传递数据
+                                vnode.attrs.setOption({
+                                    showMenu: false,
+                                    currentId: item.id
+                                });
                             }
                         }, [
-                            m('span', { class: `my-menu-label` }, item.label),
-                            m('i', { class: `my-menu-icon iconfont icon-fabijiaoyiwancheng ${model.curId === item.id ? '' : 'is-hidden'}` }) // icon
+                            m('span', { class: `my-menu-label` }, item.render ? item.render() : item.label),
+                            vnode.attrs.showMenuIcon ? m('i', { class: `my-menu-icon iconfont icon-fabijiaoyiwancheng ${vnode.attrs.currentId === item.id ? '' : 'is-hidden'}` }) : ""
                         ]);
                     })
                 )
