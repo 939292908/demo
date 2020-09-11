07d97d9f3232bf149f0bbdc27cd278fc732d03d6
HaoHaoP <HaoHaoP@HaoHaoP.com>
2020-09-11 21:38:39 +0800

    绑定手机邮箱路由跳转

--- src/views/page/user/bind/bindEmail/bindEmail.view.js
+++ src/views/page/user/bind/bindEmail/bindEmail.view.js
@@ -35,7 +35,7 @@ module.exports = {
                         m('span', {
                             class: `mr-7`,
                             onclick: () => {
-                                window.router.go(-1);
+                                window.router.push('/securityManage');
                             }
                         }, [
                             m('i.iconfont.icon-Return.iconfont-medium', {}, [])
