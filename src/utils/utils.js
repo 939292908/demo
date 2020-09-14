94f7aae5a55ba165154c2f2caf55e6837a554a43
maoxs9257 <52234904+maoxs9257@users.noreply.github.com>
2020-09-14 16:15:20 +0800

    修改交易类型判断

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1157,10 +1157,10 @@ utils.getDirByStrType = function(item){
     }else if(item.SzCls == 0 && item.SzOpn != 0){
         if (item.Dir == 1) return gDI18n.$t('10447')//"买入开多"
         if (item.Dir == -1) return gDI18n.$t('10448')//"卖出开空"
-    }else if(item.SzCls != 0 && Math.abs(item.QtyF) == Math.abs(item.SzCls)){
+    }else if(item.SzCls != 0 && Math.abs(item.QtyF || item.Sz) == Math.abs(item.SzCls)){
         if (item.Dir == 1) return gDI18n.$t('10449')//"买入平空"
         if (item.Dir == -1) return gDI18n.$t('10450')//"卖出平多"
-    }else if(item.SzCls != 0 && Math.abs(item.QtyF) != Math.abs(item.SzCls)){
+    }else if(item.SzCls != 0 && Math.abs(item.QtyF || item.Sz) != Math.abs(item.SzCls)){
         if (item.Dir == 1) return gDI18n.$t('10455')//"买入平空并开多"
         if (item.Dir == -1) return gDI18n.$t('10456')//"卖出平多并开空"
     }else if(item.via == 4 || item.via == 13){
