a69a72dfb156a007267f5d3407d4b40253e5f06e
leezCtt <leez1212@163.com>
2020-09-16 15:47:59 +0800

    #统一处理 未绑定情况

---
+++ src/views/components/theBindingOrNot/theBindingOrNot.view.js
@@ -0,0 +1,16 @@
+const m = require('mithril');
+
+module.exports = {
+    oninit: function () {
+        // alert(1);
+    },
+    oncreate: function () {
+        // alert(2);
+    },
+    view: function () {
+        return m.fragment();
+    },
+    onremove: function () {
+        // alert(3);
+    }
+};
