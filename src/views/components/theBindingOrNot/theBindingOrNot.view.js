57072fdffb0f9fef1a1d07eaa277b662b0a5f655
leezCtt <leez1212@163.com>
2020-09-16 17:17:54 +0800

    # 前置条件

--- src/views/components/theBindingOrNot/theBindingOrNot.view.js
+++ src/views/components/theBindingOrNot/theBindingOrNot.view.js
@@ -1,9 +1,12 @@
 const m = require('mithril');
+const broadcast = require('@/broadcast/broadcast');
+const globalModels = require('@/models/globalModels');
 const VerifyView = require('@/views/components/dialogVerify/dialogVerify.view');
+const l180n = require('@/languages/I18n').default;
 
 module.exports = {
-    show: false,
     isChangeClose: false,
+    UserInfor: {},
     popUpData: {
         show: false,
         doubleButton: false,
@@ -18,10 +21,43 @@ module.exports = {
         doubleButtonCof: []
     },
     oninit: function () {
-        // alert(1);
+        if (Object.keys(globalModels.getAccount()).length > 0) {
+            this.handleUserCanAction(globalModels.getAccount());
+        } else {
+            broadcast.onMsg({
+                key: this.name,
+                cmd: broadcast.GET_USER_INFO_READY,
+                cb: this.handleUserCanAction.bind(this)
+            });
+        }
     },
-    oncreate: function () {
-        // alert(2);
+    handleTotalShow: function ({ content, buttonText, buttonClick, doubleButtonCof, doubleButton, isLinshiErWeiMa }) {
+        this.isChangeClose = true;
+        this.popUpData = {
+            show: true,
+            isHandleVerify: false,
+            content,
+            buttonText,
+            buttonClick,
+            doubleButtonCof,
+            doubleButton,
+            title: { text: l180n.$t('10082') /* '温馨提示' */ },
+            isLinshiErWeiMa
+        };
+        m.redraw();
+    },
+    handleUserCanAction: function (data) {
+        console.log(9999999999999999);
+        if (data.setting2fa.email !== 1) {
+            return this.handleTotalShow({ content: l180n.$t('10404') /* '提币需邮件确认，请先绑定邮箱' */, buttonText: l180n.$t('10229') /* '邮箱验证' */, buttonClick: () => { m.route.set("/my"); } });
+        }
+        const doubleButtonCof = [
+            { text: l180n.$t('10227') /* '谷歌验证' */, issolid: false, click: () => { m.route.set("/my"); } },
+            { text: l180n.$t('10228') /* '手机验证' */, issolid: true, click: () => { m.route.set("/my"); } }
+        ];
+        if (data.setting2fa.google !== 1 && data.setting2fa.phone !== 1) {
+            return this.handleTotalShow({ content: l180n.$t('10405')/* '为了您的账户安全，请先绑定手机或谷歌' */, doubleButton: true, doubleButtonCof });
+        }
     },
     handleCloseDialog: function () {
         this.show = false;
@@ -31,10 +67,14 @@ module.exports = {
     },
     view: function () {
         return m.fragment(
-            this.show ? m(VerifyView, { close: this.isChangeClose ? this.handleBack : this.handleCloseDialog, ...this.popUpData }) : null
+            this.popUpData.show ? m(VerifyView, { close: this.isChangeClose ? this.handleBack : this.handleCloseDialog, ...this.popUpData }) : null
         );
     },
     onremove: function () {
-        // alert(3);
+        broadcast.offMsg({
+            key: this.name,
+            cmd: broadcast.GET_USER_INFO_READY,
+            isall: true
+        });
     }
 };
--- src/views/page/myAssets/extractCoin/form/form.logic.js
+++ src/views/page/myAssets/extractCoin/form/form.logic.js
@@ -254,16 +254,16 @@ const extract = {
         if (this.UserInfo.setting2fa.email !== 1 || (this.UserInfo.setting2fa.google !== 1 && this.UserInfo.setting2fa.phone !== 1)) {
             return this.handleTotalShow({ content: l180n.$t('10540') /* '提币需邮件确认，请先绑定邮箱 为了您的账户安全，还需绑定手机或谷歌' */, isLinshiErWeiMa: true });
         }
-        if (this.UserInfo.setting2fa.email !== 1) {
-            return this.handleTotalShow({ content: l180n.$t('10404') /* '提币需邮件确认，请先绑定邮箱' */, buttonText: l180n.$t('10229') /* '邮箱验证' */, buttonClick: () => { m.route.set("/my"); } });
-        }
-        const doubleButtonCof = [
-            { text: l180n.$t('10227') /* '谷歌验证' */, issolid: false, click: () => { m.route.set("/my"); } },
-            { text: l180n.$t('10228') /* '手机验证' */, issolid: true, click: () => { m.route.set("/my"); } }
-        ];
-        if (this.UserInfo.setting2fa.google !== 1 && this.UserInfo.setting2fa.phone !== 1) {
-            return this.handleTotalShow({ content: l180n.$t('10405')/* '为了您的账户安全，请先绑定手机或谷歌' */, doubleButton: true, doubleButtonCof });
-        }
+        // if (this.UserInfo.setting2fa.email !== 1) {
+        //     return this.handleTotalShow({ content: l180n.$t('10404') /* '提币需邮件确认，请先绑定邮箱' */, buttonText: l180n.$t('10229') /* '邮箱验证' */, buttonClick: () => { m.route.set("/my"); } });
+        // }
+        // const doubleButtonCof = [
+        //     { text: l180n.$t('10227') /* '谷歌验证' */, issolid: false, click: () => { m.route.set("/my"); } },
+        //     { text: l180n.$t('10228') /* '手机验证' */, issolid: true, click: () => { m.route.set("/my"); } }
+        // ];
+        // if (this.UserInfo.setting2fa.google !== 1 && this.UserInfo.setting2fa.phone !== 1) {
+        //     return this.handleTotalShow({ content: l180n.$t('10405')/* '为了您的账户安全，请先绑定手机或谷歌' */, doubleButton: true, doubleButtonCof });
+        // }
     },
     oninit: function (initWType) {
         const self = this;
