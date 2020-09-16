45ea7fa2fd57dceb2ea50cb07cfe103cd01ca665
leezCtt <leez1212@163.com>
2020-09-16 16:31:05 +0800

    # 前置条件 未完成

--- src/views/components/theBindingOrNot/theBindingOrNot.view.js
+++ src/views/components/theBindingOrNot/theBindingOrNot.view.js
@@ -1,14 +1,38 @@
 const m = require('mithril');
+const VerifyView = require('@/views/components/dialogVerify/dialogVerify.view');
 
 module.exports = {
+    show: false,
+    isChangeClose: false,
+    popUpData: {
+        show: false,
+        doubleButton: false,
+        isHandleVerify: false,
+        title: {
+            logo: '',
+            text: ''
+        },
+        content: '',
+        buttonText: '',
+        buttonClick: null,
+        doubleButtonCof: []
+    },
     oninit: function () {
         // alert(1);
     },
     oncreate: function () {
         // alert(2);
     },
+    handleCloseDialog: function () {
+        this.show = false;
+    },
+    handleBack: function () {
+        window.router.go(-1);
+    },
     view: function () {
-        return m.fragment();
+        return m.fragment(
+            this.show ? m(VerifyView, { close: this.isChangeClose ? this.handleBack : this.handleCloseDialog, ...this.popUpData }) : null
+        );
     },
     onremove: function () {
         // alert(3);
