c7782c78f74737cf61225d8b8b139f55986a759a
939292908 <64248504+939292908@users.noreply.github.com>
2020-09-29 23:38:40 +0800

    formitem组件ui

--- src/views/components/common/FormItem/FormItem.view.js
+++ src/views/components/common/FormItem/FormItem.view.js
@@ -9,18 +9,16 @@ module.exports = {
         return m('div', { class: `pub-form-item is-between is-align-center border-radius-small px-3 has-bg-level-1 ${vnode.attrs.class || ''}` },
             vnode.attrs.content ? vnode.attrs.content : [
                 m('div', { class: `no-wrap has-text-level-1` }, typeof vnode.attrs.label === 'function' ? vnode.attrs.label() : (vnode.attrs.label || '')),
-                m('div', { class: `is-between is-align-center` }, [
-                    m('input', {
-                        class: `input has-text-right has-text-level-1`,
-                        value: vnode.attrs.value,
-                        type: vnode.attrs.type || 'text',
-                        placeholder: vnode.attrs.placeholder || '',
-                        oninput(e) {
-                            vnode.attrs.updateOption({ value: e.target.value });
-                        }
-                    }),
-                    m('div', { class: `pub-form-item-unit no-wrap has-text-level-1`, onclick() { vnode.attrs.unitClick && vnode.attrs.unitClick(); } }, vnode.attrs.unit || '')
-                ])
+                m('input', {
+                    class: `input has-text-right has-text-level-1`,
+                    value: vnode.attrs.value,
+                    type: vnode.attrs.type || 'text',
+                    placeholder: vnode.attrs.placeholder || '',
+                    oninput(e) {
+                        vnode.attrs.updateOption({ value: e.target.value });
+                    }
+                }),
+                m('div', { class: `pub-form-item-unit no-wrap has-text-level-1 has-text-right`, onclick() { vnode.attrs.unitClick && vnode.attrs.unitClick(); } }, vnode.attrs.unit || '')
             ]);
     }
 };
