4c8b92976fb777582ce3129ddc215b65500eae0a
939292908 <939292908@qq.com>
2020-10-14 16:33:24 +0800

    头部组件添加类名功能

--- src/views/components/common/Header/Header.view.js
+++ src/views/components/common/Header/Header.view.js
@@ -6,14 +6,14 @@ module.exports = {
         return m('div', { class: `pub-header is-between is-align-center ${vnode.attrs.class || ''}` }, [
             // 左
             m('button.button', { onclick() { vnode.attrs.left?.onclick && vnode.attrs.left?.onclick(); } }, [
-                vnode.attrs.left?.label || m('i', { class: `iconfont icon-arrow-left has-text-level-1` })
+                vnode.attrs.left?.label || m('i', { class: `iconfont icon-arrow-left has-text-level-1 ${vnode.attrs.left?.class ? vnode.attrs.left.class : ''} ` })
             ]),
             // 中
-            m('button.button.has-text-level-1', { onclick() { vnode.attrs.center?.onclick && vnode.attrs.center?.onclick(); } }, [
+            m('button.button.has-text-level-1', { class: `${vnode.attrs.center?.class ? vnode.attrs.center.class : ''}`, onclick() { vnode.attrs.center?.onclick && vnode.attrs.center?.onclick(); } }, [
                 vnode.attrs.center?.label || ''
             ]),
             // 右
-            m('button.button', { class: `pr-3 ${vnode.attrs.right?.loading ? 'is-loading' : ''}`, onclick() { vnode.attrs.right?.onclick && vnode.attrs.right?.onclick(); } }, [
+            m('button.button', { class: `pr-3 ${vnode.attrs.right?.class ? vnode.attrs.right.class : ''} ${vnode.attrs.right?.loading ? 'is-loading' : ''}`, onclick() { vnode.attrs.right?.onclick && vnode.attrs.right?.onclick(); } }, [
                 vnode.attrs.right?.label || ''
             ])
         ]);
