8a7121925f4bd0929ac86b945de9f2091d0ca419
HT8080 <haotong0119@outlook.com>
2020-10-14 17:26:32 +0800

    新增跟单相关数据计算库

--- src/models/futureCalc/calcFuture.js
+++ src/models/futureCalc/calcFuture.js
@@ -394,3 +394,124 @@ export function calcfutureWallet(wallets, posArr, UPNLPrzActive, sumMI, UsedWltB
         wallet.walletRate = 1 - AvailBal / WltBal;
     }
 }
+
+/**
+ * 跟单数据计算主函数
+ * @param {Array} posArr 持仓数据，数组
+ * @param {Array} wallets 合约钱包数据，数组
+ * @param {Array} orderArr 合约未完成报单数据，数组
+ * @param {Object} RSObj 基础风险限额数据，Sym做key
+ * @param {Object} assetD 合约详情，Sym做key
+ * @param {Object} lastTick 最新tick行情，Sym做key
+ * @param {String} UPNLPrzActive 未实现盈亏计算选择，'1':最新价， '2'：标记价
+ * @param {String} MMType 保证金率公式选择，0: 默认，1: 开仓价值/杠杆
+ * @param {String} PrzLiqType 强平价计算公式选择，0: 默认，1: 默认公式中的MMR风险修改为MAX(MIRMy/2，MMR风险)
+ * @param {Function} cb
+ * 用该函数处理完成后各个参数新增字段如下：
+ * posArr各个持仓新增：
+ *    aMIR：风险限额，aMMR：风险限额，aSzNetLong：当前委托净买量，aSzNetShort：当前委托净卖量，aMM：仓位保证金，
+ *    aUrP：仓位未实现盈利，aUrl：仓位未实现亏损，aUPNLforLast：仓位未实现盈亏（最新价），aUPNLforM： 仓位未实现盈亏（标记价），
+ *    aProfitPer：合约回报率，aLeverEffective：仓位实际杠杆，aAdjustLevelMax仓位最大可调杠杆，仓位最小可调杠杆，
+ *    aPrzLiq：强平价格，aMgnRateforPrzM：保证金率（实际值），aMgnRateforLiq：保证金率(参考值)，aAvailMgnISO：逐仓可取保证金
+ * wallets中各个币种新增：
+ *    aUPNL: 总未实现盈亏
+ */
+export function calcFutureWltAndPosAndMIFollow(posArr, wallets, orderArr, RSObj, assetD, lastTick, UPNLPrzActive, MMType, PrzLiqType, cb) {
+    // 缺少字段补充
+    // library.addFieldForPosAndWltAndOrd(posArr, wallets, orderArr, assetD)
+
+    // let orderForPid = library.orderArrToPId(orderArr);
+    // let wallet_obj = library.walletArrToSym(wallets)
+    const sumMI = {};
+    for (let i = 0; i < posArr.length; i++) {
+        const pos = posArr[i];
+        // let orderForPidArr = orderForPid[pos.PId]||[],tick = lastTick[pos.Sym] || {}, ass = assetD[pos.Sym] || {},RS = RSObj[pos.Sym]||{}
+        // let {orderBuy, orderSell, QtyBuy, QtySell} = library.orderSplitAndSort(orderForPidArr)
+        const ass = assetD[pos.Sym] || {};
+        const SettleCoin = ass.SettleCoin;
+        const tick = lastTick[pos.Sym] || {};
+        const SettPrz = tick.SettPrz || 0;//, LastPrz = tick.LastPrz || 0, Sz = 0, SettleCoin = ass.SettleCoin
+        const RS = RSObj[pos.Sym] || {};
+
+        // let {aMIR, aMMR, aSzNetLong, aSzNetShort} = library.calcMMRAndMIR(pos.StopP || pos.StopL, orderBuy, orderSell, pos.Sz, RS)
+        const aMIR = RS.BaseMIR; const aMMR = RS.BaseMMR;
+
+        // let aMI = library.calcOrderMI(pos.StopP || pos.StopL, orderBuy, orderSell, SettPrz, ass, aMIR, pos.Lever, pos.Sz, null, pos.MIRMy)
+        const aMI = 0;
+        pos.aMI = aMI;
+        pos.aMIR = aMIR;
+        pos.aMMR = aMMR;
+        pos.aSzNetLong = 0;
+        pos.aSzNetShort = 0;
+        pos.aQtyBuy = 0;
+        pos.aQtySell = 0;
+        pos.aMM = pos.MI;
+        pos.aLeverEffective = pos.Lever;
+        pos.aUrP = pos.PNLISO > 0 ? pos.PNLISO : 0;
+        pos.aUrl = pos.PNLISO < 0 ? pos.PNLISO : 0;
+        pos.aPrzLiq = pos.PrzLiq;
+        pos.aAvailMgnISO = 0;
+
+        if (!sumMI[SettleCoin]) {
+            sumMI[SettleCoin] = 0;
+        }
+        sumMI[SettleCoin] += aMI;
+
+        // let ass = assetD[pos.Sym] || {}
+        const isReverse = (ass.Flag & 1) === 1 || ass.TrdCls === 2 ? 1 : 0;
+        const ValIni = library.calcVal(isReverse, pos.PrzIni, pos.Sz, (ass.LotSz || 0));
+        const ValM = library.calcVal(isReverse, tick.SettPrz, pos.Sz, (ass.LotSz || 0));
+        const ValL = library.calcVal(isReverse, tick.LastPrz, pos.Sz, (ass.LotSz || 0));
+
+        // let MM = 0, _MM = 0, UPNLforM = 0, UPNLforLast = 0, UrP = 0, UrL = 0, profitPer = 0, AvailMgnISO = 0;
+        pos.aValIni = ValIni;
+        pos.aUPNLforLast = ValL - ValIni;
+        pos.aUPNLforM = ValM - ValIni;
+
+        const MgnCalcAndMgnISO = Math.abs(ValIni) / Math.min(pos.Lever, 1 / pos.aMIR);
+        const MM = MgnCalcAndMgnISO + pos.PNLISO;
+        if (UPNLPrzActive === '1') {
+            pos.aProfitPer = pos.aUPNLforLast / MM;
+        } else if (UPNLPrzActive === '2') {
+            pos.aProfitPer = pos.aUPNLforM / MM;
+        }
+
+        if (isReverse) {
+            pos.aMgnRateforPrzM = SettPrz ? 1 - pos.PrzIni / SettPrz : 0;
+            pos.aMgnRateforLiq = 1 - pos.PrzIni / pos.PrzLiq;
+        } else {
+            pos.aMgnRateforPrzM = SettPrz / pos.PrzIni - 1;
+            pos.aMgnRateforLiq = pos.PrzLiq / pos.PrzIni - 1;
+        }
+    }
+
+    calcfutureWalletFollow(wallets, posArr, UPNLPrzActive, assetD);
+    const param = { posArr };
+    cb && cb(param);
+}
+
+/**
+ * 跟单钱包数据计算
+ * @param {Array} wallets
+ * @param {Array} posArr
+ * @param {Object} sumMI
+ * @param {Object} UsedWltBal
+ * @param {Object} assetD
+ * 计算后钱包数据新增字段，MgnBal:保证金余额，aMM:总仓位保证金 , aMI:总委托保证金, aUPNL: 总未实现盈亏, aGift可用赠金, aWdrawable可用保证金， maxTransfer最大划转
+ */
+export function calcfutureWalletFollow(wallets, posArr, UPNLPrzActive, assetD) {
+    for (let i = 0; i < wallets.length; i++) {
+        const wallet = wallets[i]; let aUPNL = 0;
+        for (let i = 0; i < posArr.length; i++) {
+            const pos = posArr[i];
+            if (pos.WId === wallet.wType) {
+                if (UPNLPrzActive === '1') {
+                    aUPNL += (pos.aUPNLforLast || 0);
+                } else if (UPNLPrzActive === '2') {
+                    aUPNL += (pos.aUPNLforM || 0);
+                }
+            }
+        }
+        wallet.aUPNL = Number(aUPNL).toFixed(8);
+    }
+}
