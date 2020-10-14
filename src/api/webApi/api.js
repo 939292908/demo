f11d5f086b27be07ba3c1866f992c24d4441450a
HT8080 <haotong0119@outlook.com>
2020-10-14 17:37:11 +0800

    修改跟单仓位接口参数说明

--- src/api/webApi/api.js
+++ src/api/webApi/api.js
@@ -814,12 +814,17 @@ export function followTransrer (params = {}, options = { withCredentials: false 
     return Http.post(API.FORDER_TRANSFER, params, options);
 }
 
-/*
-* 获取跟单持仓
-* @param params {
-*   positions:[] // 当前已存在的仓位pid，数组形式
-* }
-* */
+/**
+ * 获取跟单持仓
+ * @param {*} params {
+ *   positions:[] // 当前已存在的仓位pid，数组形式
+ * }
+ * @returns {Object} {
+        "code":0,
+        "data":[], // 新增的仓位
+        "delPos":[] // 删除的仓位
+    }
+ */
 export function getFollowPosition (params = {}, options = { withCredentials: false }) {
     return Http.post(API.FORDER_GETPOSITION_V1, params, options);
 }
