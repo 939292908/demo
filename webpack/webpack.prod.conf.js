b237d48ab86b66e52209d6ce85ecb21172844efa
939292908 <939292908@qq.com>
2020-10-22 00:00:42 +0800

    publicPath修改

--- webpack/webpack.prod.conf.js
+++ webpack/webpack.prod.conf.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const Uglifyjs = require('uglifyjs-webpack-plugin');
 
-const publicPath = "https://static.abkjl.com/vp3/m/";
+const publicPath = "https://static.abkjl.com/vp3/m/redpacket/";
 
 module.exports = {
     mode: "production",
