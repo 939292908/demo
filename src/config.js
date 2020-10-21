c077c588c15ade0906f9d83009e5eb1ee611c1c8
939292908 <939292908@qq.com>
2020-10-21 15:38:20 +0800

    主红包config配置修改

--- src/config.js
+++ src/config.js
@@ -9,30 +9,9 @@ module.exports = {
     exchInfo: {
         exchCoin: "",
         exchLogo: "",
-        leverStatus: 0,
         helpCenter: {
             langs: { zh: "zh-cn", tw: "zh-cn", en: "en-001" },
-            website: "https://xmexhelp.zendesk.com",
-            termsServiceId: "360040811494",
-            privacyPolicyId: "360041288893",
-            futureReferenceId: "360040901334",
-            followIdentityExplain: "360008615533",
-            followNewbie: "360008486274"
-        },
-        warrants: "",
-        downloadType: 0,
-        hideDownload: false,
-        bottomNavHomeIcon: "icon-nav-ex21",
-        hideWXShareBtn: false,
-        openGuidePage: 0,
-        guidePageVer: "",
-        customIcon: 0,
-        isKlineDark: false,
-        klineTargetListType: 1
-    },
-    future: {
-        UPNLPrzActive: '2', // 未实现盈亏计算选择，'1':最新价， '2'：标记价
-        MMType: 1, // 保证金率公式选择，0: 默认，1: 开仓价值/杠杆
-        PrzLiqType: 0 // 强平价计算公式选择，0: 默认，1: 默认公式中的MMR风险修改为MAX(MIRMy/2，MMR风险)
+            website: "https://xmexhelp.zendesk.com"
+        }
     }
 };
